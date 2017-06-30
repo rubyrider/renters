6d0a3bfdd9808fb7b3c2f1cb5f5ba48d129159ba
Irfan Ahmed <irfan@lesouk.co>
2017-06-30 13:27:45 +0600

    Fix property routing issue

--- app/controllers/dashboard/properties_controller.rb
+++ app/controllers/dashboard/properties_controller.rb
@@ -31,7 +31,7 @@ module Dashboard
 
       respond_to do |format|
         if @property.save
-          format.html { redirect_to url_for(action: :show), notice: 'Property was successfully created.' }
+          format.html { redirect_to [:dashboard, @property], notice: 'Property was successfully created.' }
           format.json { render :show, status: :created, location: @property }
         else
           format.html { render :new }
@@ -45,7 +45,7 @@ module Dashboard
     def update
       respond_to do |format|
         if @property.update(property_params)
-          format.html { redirect_to url_for(action: :show), notice: 'Property was successfully updated.' }
+          format.html { redirect_to [:dashboard, @property], notice: 'Property was successfully updated.' }
           format.json { render :show, status: :ok, location: @property }
         else
           format.html { render :edit }
@@ -59,7 +59,7 @@ module Dashboard
     def destroy
       @property.destroy
       respond_to do |format|
-        format.html { redirect_to url_for(action: :index), notice: 'Property was successfully destroyed.' }
+        format.html { redirect_to properties_url, notice: 'Property was successfully destroyed.' }
         format.json { head :no_content }
       end
     end
